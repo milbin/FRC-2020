d6343157080700904613511dd75573cce2b76b65
Julian Curalli <julian.curalli27@gmail.com>
2020-01-18 19:11:55 -0500

    Update RamseteControlCommand.java

--- src/main/java/frc/robot/commands/RamseteControlCommand.java
+++ src/main/java/frc/robot/commands/RamseteControlCommand.java
@@ -62,6 +62,6 @@ public class RamseteControlCommand extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
